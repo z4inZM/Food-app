70719a43c377e9160c1d9f68c0b19c38effeaedc
Zain Ul Abideen <uzain9661@gmail.com>
2024-10-19 14:22:05 +0500

    commit

---
+++ Frontend/src/Components/Header/Header.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import './Header.css'
+const Header = () => {
+  return (
+    <div className='header'>
+        <div className="header-contents">
+
+            <h2>
+                Order your Favourite Food Here
+            </h2>
+            <p>
+            Craving Something Delicious? Order Your Favorite Food Here and Enjoy Fast, Fresh, and Flavorful Meals Delivered to Your Doorstep!
+                
+            </p>
+         <button className='button'>View Menu</button>
+        </div>
+        </div>
+  )
+}
+
+export default Header
